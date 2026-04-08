8bf2afef2244f098f79c05ed66bf3e1a389b66d0
Rafli Pratama <rflipratm@gmail.com>
2026-04-08 10:55:43 +0700

    feat: menambahkan fitur baru

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,15 +2,17 @@ import { render } from "../config/viewEngine";
 import * as mahasiswaModel from "../models/mahasiswaModel";
 
 export const home = async (c) => {
-  // Ambil jumlah total mahasiswa dari database
+  // Ambil jumlah total dan list mahasiswa dari database
   const total_mahasiswa = await mahasiswaModel.count();
+  const mahasiswa = await mahasiswaModel.getAll();
 
   const html = await render(
     "home",
     {
       title: "Dashboard Bun MVC",
       message: "Hello dari Bun + Tailwind",
-      total_mahasiswa, // Kirim datanya kesini
+      total_mahasiswa,
+      mahasiswa, // <--- Kirim datanya kesini
     },
     c,
   );
